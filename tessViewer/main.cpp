6ab1667d14d92b64af0fba812367b87b4c8e3d2d
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-11-11 23:27:23 +0900

    変更 コマンドライン引数をConfigで管理する

--- tessViewer/main.cpp
+++ tessViewer/main.cpp
@@ -5,12 +5,17 @@
 #include <fstream>
 
 #include <boost\program_options.hpp>
+#include <string_view>
 
+#include "boost/program_options/variables_map.hpp"
 #include "location_define.h"
+#include "nlohmann/json_fwd.hpp"
 #include "version.h"
 
 #include "glapp\config.h"
 #include "glapp\glapp_define.h"
+
+void import_options(const boost::program_options::variables_map& vm);
 
 int main(int argc, char* argv[]) {
 	using namespace std::literals::string_literals;
@@ -20,37 +25,31 @@ int main(int argc, char* argv[]) {
 	options_description graphics_option("graphics_option");
 	options_description ui_option("ui_option");
 
-	options.add_options()("help,h", "ヘルプを表示")("version,v",
-													"バージョン情報")(
-		"object,o", value<std::string>()->default_value(MODEL "cube_uv.sdmj"),
-		"表示モデル");
+	options.add_options()("help,h", "ヘルプを表示")(
+		"version,v", "バージョン情報")("object,o", value<std::string>(),
+									   "表示モデル");
 
-	window_option.add_options()(GLAPP_CONFIG_FULLSCREEN ",f", "全画面表示")(
-		GLAPP_CONFIG_RESOLUTION_X, value<int>()->default_value(1280),
-		"横解像度")(GLAPP_CONFIG_RESOLUTION_Y, value<int>()->default_value(720),
-					"縦解像度")(GLAPP_CONFIG_FOV,
-								value<float>()->default_value(60.f), "Fov")(
-		GLAPP_CONFIG_VSYNC, value<bool>()->default_value(true), "VSync");
+	window_option.add_options()(GLAPP_CONFIG_FULLSCREEN ",f", value<bool>(),
+								"全画面表示")(GLAPP_CONFIG_RESOLUTION_X,
+											  value<int>(), "横解像度")(
+		GLAPP_CONFIG_RESOLUTION_Y, value<int>(),
+		"縦解像度")(GLAPP_CONFIG_FOV, value<float>(),
+					"Fov")(GLAPP_CONFIG_VSYNC, value<bool>(), "VSync");
 
 	graphics_option.add_options()(GLAPP_CONFIG_PATCH_TYPE_GREGORY ",g",
 								  "Patch Type に GREGORY_BASIS を使用")(
-		GLAPP_CONFIG_PATCH_LEVEL_DEFAULT ",p", value<int>()->default_value(2),
-		"デフォルトパッチレベル")(GLAPP_CONFIG_PATCH_LEVEL_MAX,
-								  value<int>()->default_value(6),
+		GLAPP_CONFIG_PATCH_LEVEL_DEFAULT ",p", value<int>(),
+		"デフォルトパッチレベル")(GLAPP_CONFIG_PATCH_LEVEL_MAX, value<int>(),
 								  "最大パッチレベル")(
-		GLAPP_CONFIG_TESS_LEVEL_DEFAULT ",t", value<int>()->default_value(1),
-		"デフォルトテッセレーション係数")(GLAPP_CONFIG_TESS_LEVEL_MAX,
-										  value<int>()->default_value(6),
-										  "最大テッセレーション係数");
+		GLAPP_CONFIG_TESS_LEVEL_DEFAULT ",t", value<int>(),
+		"デフォルトテッセレーション係数")(
+		GLAPP_CONFIG_TESS_LEVEL_MAX, value<int>(), "最大テッセレーション係数");
 
-	ui_option.add_options()(GLAPP_CONFIG_USER_INTERFACE,
-							value<bool>()->default_value(true),
+	ui_option.add_options()(GLAPP_CONFIG_USER_INTERFACE, value<bool>(),
 							"ユーザーインターフェイス")
 		//(GLAPP_CONFIG_FONT_FILE, value<std::string>()->default_value(FONT R"(ipaexg.ttf)"), R"(使用するフォント)")
-		(GLAPP_CONFIG_FONT_SIZE, value<int>()->default_value(16),
-		 "フォントサイズ")(GLAPP_CONFIG_FONT_COLOR,
-						   value<std::uint8_t>()->default_value(0),
-						   "フォント色");
+		(GLAPP_CONFIG_FONT_SIZE, value<int>(), "フォントサイズ")(
+			GLAPP_CONFIG_FONT_COLOR, value<std::uint8_t>(), "フォント色");
 
 	options.add(window_option).add(graphics_option).add(ui_option);
 
@@ -98,4 +97,53 @@ int main(int argc, char* argv[]) {
 	catch (...) {
 		std::cerr << "Unknown Exception\n" << std::endl;
 	}
+}
+
+void import_options(const boost::program_options::variables_map& vm) {
+	using nlohmann::json;
+	json options;
+	json window;
+	json graphics;
+	json ui;
+	auto contain = [&](std::string_view name) -> bool {
+		return vm.count(std::string(name)) > 0;
+	};
+
+	if (contain(GLAPP_CONFIG_FULLSCREEN)) {
+		window["fullscreen"] = vm[GLAPP_CONFIG_FULLSCREEN].as<bool>();
+	}
+	if (contain(GLAPP_CONFIG_RESOLUTION_X)) {
+		window["/resolution/width"] = vm[GLAPP_CONFIG_RESOLUTION_X].as<int>();
+	}
+	if (contain(GLAPP_CONFIG_RESOLUTION_Y)) {
+		window["/resolution/height"] = vm[GLAPP_CONFIG_RESOLUTION_Y].as<int>();
+	}
+	if (contain(GLAPP_CONFIG_FOV)) {
+		window["fov"] = vm[GLAPP_CONFIG_FOV].as<float>();
+	}
+	if (contain(GLAPP_CONFIG_VSYNC)) {
+		window["vsync"] = vm[GLAPP_CONFIG_VSYNC].as<bool>();
+	}
+	if (contain("object")) {
+		graphics["/graphics/model"] = vm["object"].as<std::string>();
+	}
+	if (contain(GLAPP_CONFIG_PATCH_LEVEL_DEFAULT)) {
+		graphics["/graphics/osd/patch/level"] =
+			vm[GLAPP_CONFIG_PATCH_LEVEL_DEFAULT].as<int>();
+	}
+	if (contain(GLAPP_CONFIG_TESS_LEVEL_DEFAULT)) {
+		graphics["/graphics/osd/tessellation/level"] =
+			vm[GLAPP_CONFIG_TESS_LEVEL_DEFAULT].as<int>();
+	}
+	if (contain(GLAPP_CONFIG_FONT_SIZE)) {
+		ui["/ui/font/size"] = vm[GLAPP_CONFIG_FONT_SIZE].as<int>();
+	}
+	if (contain(GLAPP_CONFIG_FONT_COLOR)) {
+		ui["/ui/font/color"] = vm[GLAPP_CONFIG_FONT_COLOR].as<std::string>();
+	}
+	options["window"]   = window;
+	options["graphics"] = graphics;
+	options["ui"]       = ui;
+
+	glapp::Config::CommandLineOptions(options);
 }
