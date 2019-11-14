5149b5982c58869b2e96f0ddad3d3c5e12ee7cb9
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-11-14 23:38:33 +0900

    追加 jsonシリアライズ関数

--- tessViewer/camera.h
+++ tessViewer/camera.h
@@ -37,4 +37,33 @@ protected:
 	static constexpr glm::vec3 up    = {1, 0, 0};
 	static constexpr glm::vec3 right = {0, 1, 0};
 };
+template <class BasicJsonType>
+void to_json(BasicJsonType& j, const Camera& cam) {
+	j["position"]  = cam._pos;
+	j["angle"]     = glm::degrees(glm::eulerAngles(cam._quat));
+	j["lookpoint"] = cam._lookpoint;
+	j["fov"]       = cam._fov;
+	j["near"]      = cam._near;
+	j["far"]       = cam._far;
+}
+template <class BasicJsonType>
+void from_json(const BasicJsonType& j, Camera& cam) {
+	auto schema    = glapp::Config::Get().Schema("/graphics/camera");
+	cam._pos       = j.at("position").get<glm::vec3>();
+	cam._quat      = glm::quat(j.at("angle").get<glm::vec3>());
+	cam._lookpoint = cam._quat * Camera::front;
+	cam._right     = cam._quat * Camera::right;
+	cam._up        = cam._quat * Camera::up;
+	cam._fov       = std::clamp(j.at("fov").get<float>(),
+                          schema.at("property/fov/minimum").get<float>(),
+                          schema.at("property/fov/maximum").get<float>());
+	if (auto near = j.at("near").get<float>(), far = j.at("far").get<float>();
+		near < far) {
+		cam._near = j.at("near").get<float>();
+		cam._far  = j.at("far").get<float>();
+	} else {
+		cam._near = schema.at("property/near/default").get<float>();
+		cam._far  = schema.at("property/far/default").get<float>();
+	}
+}
 } // namespace tv
