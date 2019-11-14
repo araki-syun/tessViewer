a4f57fc4bf8bae63d8aaa552f3275266e85b3944
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-11-14 23:37:54 +0900

    追加 Cameraクラスファイルを分離

--- tessViewer/camera.h
+++ tessViewer/camera.h
@@ -1,18 +1,40 @@
 #pragma once
 
+#include "glm/fwd.hpp"
+#include "nlohmann/json_fwd.hpp"
+#include <algorithm>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include "glapp/glm_conversion.h"
+#include "glapp/config.h"
 
 namespace tv {
 class Camera {
-	glm::vec3 Pos;
-	glm::vec3 Angle;
-	glm::vec3 LookPoint;
-	glm::vec3 Right;
-	glm::vec3 Up;
-	float     Fov;
-	float     Near;
-	float     Far;
-	glm::vec2 Move;
-	bool      FlyMode;
+	// template <class BasicJsonType>
+	friend void to_json(nlohmann::json& j, const Camera& cam);
+	// template <class BasicJsonType>
+	friend void from_json(const nlohmann::json& j, Camera& cam);
+
+public:
+	Camera();
+
+protected:
+	glm::vec3 _pos;
+	glm::quat _quat;
+	glm::vec3 _lookpoint;
+	glm::vec3 _right;
+	glm::vec3 _up;
+	float     _fov;
+	float     _near;
+	float     _far;
+
+	static float _max_fov;
+	static float _min_fov;
+
+	static constexpr glm::vec3 front = {0, 0, -1};
+	static constexpr glm::vec3 up    = {1, 0, 0};
+	static constexpr glm::vec3 right = {0, 1, 0};
 };
 } // namespace tv
