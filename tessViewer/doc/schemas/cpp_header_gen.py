98f51d2453fa4b7b15d34e27f3a173a9114d89d6
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-11-28 16:24:38 +0900

    追加 jsonスキーマ＆生成スクリプト

--- tessViewer/doc/schemas/cpp_header_gen.py
+++ tessViewer/doc/schemas/cpp_header_gen.py
@@ -1,16 +1,26 @@
 import os
+import sys
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--indent",
+                    type=int,
+                    default=0,
+                    help="indent width")
+args = parser.parse_args()
 
 directory = os.path.dirname(__file__)
 schema = directory + r"/setting_schema.json"
 out = schema + r".gen.h"
 
 with open(out, "w", encoding="utf-8") as h:
-#    h.write(r"#pragma once" + "\n")
-#    h.write(r"namespace glapp::schema{")
-    h.write(r"constexpr char setting_json[] =" + "\n")
+    h.write("constexpr char setting_json[] =\nR\"(")
     with open(schema, "r", encoding="utf-8") as f:
-        for line in f:
-            s = line.replace(r'"', r'\"').replace("\n","")
-            h.write(f'\"{s}\"\n')
-    h.write(r";")
-#    h.write(r"} // namespace glapp::schema")
+        j = json.load(f)
+        if args.indent == 0:
+            h.write(json.dumps(j))
+        else:
+            h.write(json.dumps(j, indent=args.indent))
+    h.write(")\";")
+sys.exit()
