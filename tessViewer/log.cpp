38d08ff49070768f6f4d344b239420a9341fac7f
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-12-02 19:02:33 +0900

    追加 ログ関数のレベル表示

--- tessViewer/log.cpp
+++ tessViewer/log.cpp
@@ -64,9 +64,9 @@ void Logger::_log(LogLevel lv, InfoType type, std::string_view str) {
 	auto    now = std::time(nullptr);
 	std::tm tm{};
 	localtime_s(&tm, &now);
-	_output << fmt::format("[{:s}] {:%F %T} {:s}\n",
-						   ToString(type), //NOLINTNEXTLINE
-						   tm, std::string(str))
+	_output << fmt::format("[{:s}] {:%F %T} [{:s}] {:s}",
+						   ToString(type), 
+						   tm, ToString(lv), std::string(str))
 			<< std::endl;
 }
 } // namespace tv
