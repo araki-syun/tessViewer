5d1e8912a49ce5ef62658bb90b660e70c6ecd2e9
Syun Araki <46486848+araki-syun@users.noreply.github.com>
2019-12-29 21:44:22 +0900

    変更 バージョンをCMakeLists.txtで指定

--- tessViewer/version.h
+++ tessViewer/version.h
@@ -1,3 +1,6 @@
 #pragma once
 
-constexpr auto TV_VERSION = "0.2.0";
+constexpr auto TV_VERSION       = "0.2.0";
+constexpr int  TV_VERSION_MAJOR = 0;
+constexpr int  TV_VERSION_MINOR = 2;
+constexpr int  TV_VERSION_PATCH = 0;
